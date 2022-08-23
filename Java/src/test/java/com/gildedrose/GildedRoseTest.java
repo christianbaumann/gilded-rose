4c5be004c73a7fa71792c09b89e191f3030a26bc
Chriss <chriss713@gmail.com>
2022-08-23 18:04:47 +0200

    Added testNormalItemBeforeSellDate()

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,10 +10,24 @@ class GildedRoseTest {
     void foo() {
         Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
+
         assertEquals("foo", app.items[0].name);
         assertEquals(-1, app.items[0].sellIn);
         assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void testNormalItemBeforeSellDate() {
+        Item[] items = new Item[]{new Item("normal", 10, 5)};
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("normal", app.items[0].name);
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(4, app.items[0].quality);
     }
 
 }
