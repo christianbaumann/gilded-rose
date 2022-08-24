a4d2192574860c3eebdc44e8225d0f1264ae76d4
Chriss <chriss713@gmail.com>
2022-08-24 07:17:18 +0200

    Added testAgedBrieQualityIncreasesBeforeSellinExpired()

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,6 +7,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
+    void testAgedBrieQualityIncreasesBeforeSellinExpired() {
+        Item[] items = new Item[]{
+            new Item("Aged Brie", 5, 10),
+            new Item("Aged Brie", 1, 12),
+            new Item("Aged Brie", 0, 14)
+        };
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(4, app.items[0].sellIn);
+        assertEquals(11, app.items[0].quality);
+
+        assertEquals("Aged Brie", app.items[1].name);
+        assertEquals(0, app.items[1].sellIn);
+        assertEquals(13, app.items[1].quality);
+
+        assertEquals("Aged Brie", app.items[2].name);
+        assertEquals(-1, app.items[2].sellIn);
+        assertEquals(16, app.items[2].quality);
+    }
+
+    @Test
     void foo() {
         Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
