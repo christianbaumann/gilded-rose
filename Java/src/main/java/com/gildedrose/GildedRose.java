acd7def1002d6ea9dbe5a76f4089d609e9f5249f
Chriss <chriss713@gmail.com>
2022-08-25 17:46:27 +0200

    Introduced updateQualityAgedBrie()

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,60 +10,83 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
 
-            if (item.name.equals("Normal")) {
-                item = updateQualityNormal(item);
-            } else {
-
-                if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-
-                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.sellIn < 11) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
+            switch (item.name) {
+                case "Aged Brie":
+                    item = updateQualityAgedBrie(item);
+                    break;
+                case "Normal":
+                    item = updateQualityNormal(item);
+                    break;
+                default:
+                    if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.quality > 0) {
+                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                item.quality = item.quality - 1;
                             }
-
-                            if (item.sellIn < 6) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.sellIn = item.sellIn - 1;
-                }
-
-                if (item.sellIn < 0) {
-                    if (!item.name.equals("Aged Brie")) {
-                        if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                            if (item.quality > 0) {
-                                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                    item.quality = item.quality - 1;
-                                }
-                            }
-                        } else {
-                            item.quality = item.quality - item.quality;
                         }
                     } else {
                         if (item.quality < 50) {
                             item.quality = item.quality + 1;
+
+                            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                                if (item.sellIn < 11) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1;
+                                    }
+                                }
+
+                                if (item.sellIn < 6) {
+                                    if (item.quality < 50) {
+                                        item.quality = item.quality + 1;
+                                    }
+                                }
+                            }
                         }
                     }
-                }
 
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.sellIn = item.sellIn - 1;
+                    }
+
+                    if (item.sellIn < 0) {
+                        if (!item.name.equals("Aged Brie")) {
+                            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                                if (item.quality > 0) {
+                                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                                        item.quality = item.quality - 1;
+                                    }
+                                }
+                            } else {
+                                item.quality = item.quality - item.quality;
+                            }
+                        } else {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
+                            }
+                        }
+                    }
             }
         }
+    }
+
+    private Item updateQualityAgedBrie(Item item) {
+        item.sellIn--;
+
+        if (item.quality >= 50) {
+            return item;
+        }
+
+        item.quality++;
+
+        if (item.quality >= 50) {
+            return item;
+        }
+
+        if (item.sellIn <= 0) {
+            item.quality++;
+        }
+
+        return item;
     }
 
     private Item updateQualityNormal(Item item) {
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -9,9 +9,7 @@ class GildedRoseTest {
     @Test
     void testAgedBrieQualityIncreasesBeforeSellinExpired() {
         Item[] items = new Item[]{
-            new Item("Aged Brie", 5, 10),
-            new Item("Aged Brie", 1, 12),
-            new Item("Aged Brie", 0, 14)
+            new Item("Aged Brie", 5, 10)
         };
         GildedRose app = new GildedRose(items);
 
@@ -20,21 +18,15 @@ class GildedRoseTest {
         assertEquals("Aged Brie", app.items[0].name);
         assertEquals(4, app.items[0].sellIn);
         assertEquals(11, app.items[0].quality);
-
-        assertEquals("Aged Brie", app.items[1].name);
-        assertEquals(0, app.items[1].sellIn);
-        assertEquals(13, app.items[1].quality);
-
-        assertEquals("Aged Brie", app.items[2].name);
-        assertEquals(-1, app.items[2].sellIn);
-        assertEquals(16, app.items[2].quality);
     }
 
     @Test
     void testAgedBrieQualityIncreasesAfterSellinExpired() {
         Item[] items = new Item[]{
             new Item("Aged Brie", -1, 16),
-            new Item("Aged Brie", -5, 18)
+            new Item("Aged Brie", -5, 18),
+            new Item("Aged Brie", 1, 12),
+            new Item("Aged Brie", 0, 14)
         };
         GildedRose app = new GildedRose(items);
 
@@ -47,6 +39,14 @@ class GildedRoseTest {
         assertEquals("Aged Brie", app.items[1].name);
         assertEquals(-6, app.items[1].sellIn);
         assertEquals(20, app.items[1].quality);
+
+        assertEquals("Aged Brie", app.items[2].name);
+        assertEquals(0, app.items[2].sellIn);
+        assertEquals(14, app.items[2].quality);
+
+        assertEquals("Aged Brie", app.items[3].name);
+        assertEquals(-1, app.items[3].sellIn);
+        assertEquals(16, app.items[3].quality);
     }
 
     @Test
